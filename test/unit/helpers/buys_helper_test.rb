6c5418691cb5b9a0160c431d0b29107a2a673041
Andrew Bloom <andrew@andrewkbloom.com>
2009-09-10 00:00:31 -0500

    a few more tests, very simple stuff

--- test/unit/helpers/buys_helper_test.rb
+++ test/unit/helpers/buys_helper_test.rb
@@ -1,4 +1,13 @@
 require 'test_helper'
 
 class BuysHelperTest < ActionView::TestCase
+  test "add_placement_path for existing buy" do
+    buy = Factory(:buy)
+    assert_equal "/buys/#{buy.id}/add_placement", add_placement_path(buy)
+  end
+  
+  test "add_placement_path for new buy" do
+    buy = Buy.new
+    assert_equal "/buys/new/add_placement", add_placement_path(buy)
+  end
 end
--- test/unit/helpers/plans_helper_test.rb
+++ test/unit/helpers/plans_helper_test.rb
@@ -1,4 +1,23 @@
 require 'test_helper'
 
 class PlansHelperTest < ActionView::TestCase
+  test "add_placement_path for existing buy" do
+    @plan = Factory(:plan)
+    assert_equal "/plans/#{@plan.buys.first.id}/add_placement?buy_index=0", add_placement_path(@plan.buys.first)
+  end
+  
+  test "add_placement_path for new buy" do
+    @plan = Plan.new(:buys => [Buy.new])
+    assert_equal "/plans/new/add_placement?buy_index=0", add_placement_path(@plan.buys.first)
+  end
+  
+  test "add_buy_path for existing plan" do
+    @plan = Factory(:plan)
+    assert_equal "/plans/#{@plan.buys.first.id}/add_buy", add_buy_path
+  end
+  
+  test "add_buy_path for new plan" do
+    @plan = Plan.new(:buys => [Buy.new])
+    assert_equal "/plans/new/add_buy", add_buy_path
+  end
 end
